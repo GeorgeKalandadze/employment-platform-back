2cfcef704fe30170de374521da7a7ccced0e6c08
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-02-16 18:49:45 +0400

    feat:add routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function() {
+    Route::post('register', 'register')->name('register');
+    Route::post('login', 'login')->name('login');
+    Route::post('email/verify', 'verify')->name('verification.verify');
+	Route::post('logout', 'logout')->name('logout');
 });
