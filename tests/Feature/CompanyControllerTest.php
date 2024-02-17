4f8636cdf32a9710cb79af701f8a3997d7e6ceea
George Kalandadze <kalandadzeg777@gmail.com>
2024-02-18 00:17:14 +0400

    feat:make feature tests for company create, update, and delete

---
+++ tests/Feature/CompanyControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class CompanyControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+    use WithFaker;
+
+    public function testStoreCompany()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $data = [
+            'user_id' => $user->id,
+            'name' => 'Test Company',
+            'logo' => UploadedFile::fake()->create('company_logo.jpg'),
+            'address' => '123 Test St',
+            'mobile_number' => '1234567890',
+            'email' => 'test@example.com',
+            'website' => 'http://example.com',
+            'description' => 'This is a test company',
+        ];
+
+        $response = $this->postJson('/api/companies', $data);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'message' => 'Company created successfully',
+
+            ]);
+    }
+
+    public function testUpdateCompany()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $company = Company::factory()->create(['user_id' => $user->id]);
+
+        $data = [
+            'user_id' => $user->id,
+            'name' => 'Test Company',
+            'logo' => UploadedFile::fake()->create('company_logo.jpg'),
+            'address' => '123 Test St',
+            'mobile_number' => '1234567890',
+            'email' => 'test@example.com',
+            'website' => 'http://example.com',
+            'description' => 'This is a test company',
+        ];
+
+        $response = $this->putJson("/api/companies/{$company->id}", $data );
+
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'Company updated successfully']);
+
+    }
+
+    public function testDeleteCompany()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $company = Company::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->deleteJson("/api/companies/{$company->id}");
+
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'Company deleted successfully']);
+
+    }
+}
