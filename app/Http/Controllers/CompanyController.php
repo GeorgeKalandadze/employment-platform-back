e223462da97b119ae0b82939c49d7b9090513be0
George Kalandadze <kalandadzeg777@gmail.com>
2024-02-17 15:43:49 +0400

    feat:make seeder for user and store user method

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CompanyRequest;
+use App\Models\Company;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CompanyRequest $request): JsonResponse
+    {
+        $user_id = 2;
+
+        $validatedData = $request->validated();
+
+        $validatedData['user_id'] = $user_id;
+
+        if ($request->hasFile('logo')) {
+            $logoPath = $request->file('logo')->store('company-logos', 'public');
+
+            $validatedData['logo'] = $logoPath;
+        }
+
+        Company::create($validatedData);
+
+        return response()->json(['message' => 'Company created successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
