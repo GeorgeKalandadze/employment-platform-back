f2398f1fed42f0ef6625f4fa58acd38acb5c1657
George Kalandadze <kalandadzeg777@gmail.com>
2024-02-21 22:32:06 +0400

    feat/write tests for toggle favorie

---
+++ tests/Feature/AddToFavoriteTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Course;
+use App\Models\User;
+use App\Models\Vacancy;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class AddToFavoriteTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testToggleFavoriteCourse()
+    {
+        $user = User::factory()->create();
+        Auth::login($user);
+        $course = Course::factory()->create();
+
+        $response = $this->postJson("/api/courses/{$course->id}/favorite");
+        $response->assertJson(['message' => 'Course added to favorites']);
+
+        $response = $this->postJson("/api/courses/{$course->id}/favorite");
+        $response->assertJson(['message' => 'Course removed from favorites']);
+    }
+
+
+    public function testToggleFavoriteVacancy()
+    {
+        $user = User::factory()->create();
+        Auth::login($user);
+        $vacancy = Vacancy::factory()->create();
+
+        $response = $this->postJson("/api/vacancies/{$vacancy->id}/favorite");
+        $response->assertJson(['message' => 'Vacancy added to favorites']);
+
+        $response = $this->postJson("/api/vacancies/{$vacancy->id}/favorite");
+        $response->assertJson(['message' => 'Vacancy removed from favorites']);
+    }
+}
