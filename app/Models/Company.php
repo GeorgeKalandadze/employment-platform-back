f1ed352d07fe768f4e2a0e09c456f42e77c0bda3
George Kalandadze <kalandadzeg777@gmail.com>
2024-02-23 15:38:12 +0400

    feat/make for follow between user and company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Company extends Model
@@ -36,4 +37,12 @@ class Company extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function followers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'company_followers', 'company_id', 'user_id')
+            ->withTimestamps();
+    }
+
+
 }
