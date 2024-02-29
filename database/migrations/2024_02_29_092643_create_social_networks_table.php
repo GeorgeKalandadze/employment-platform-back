649ab41d378076fd0e3b0a07a2f3f627ad8cc935
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-01 00:48:09 +0400

    fix/social networks migration

--- database/migrations/2024_02_29_092643_create_social_networks_table.php
+++ database/migrations/2024_02_29_092643_create_social_networks_table.php
@@ -13,9 +13,8 @@ return new class extends Migration
     {
         Schema::create('social_networks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('company_id');
             $table->string('link');
-            $table->morphs('sociable');
             $table->timestamps();
         });
     }
