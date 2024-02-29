b09367d225df1882dc4124de9097b68f71544a1c
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-01 01:55:35 +0400

    feat/add fillable property for socialNetwork model

--- app/Models/SocialNetwork.php
+++ app/Models/SocialNetwork.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SocialNetwork extends Model
 {
-    
+    protected $fillable = ['company_id', 'link'];
 
     public function companies(): BelongsTo
     {
