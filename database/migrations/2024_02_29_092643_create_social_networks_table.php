3922da3a739522c48b85a53c012a729f9268d3e6
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-01 02:10:02 +0400

    refactor/company_id foreign key to use cascade deletion

--- database/migrations/2024_02_29_092643_create_social_networks_table.php
+++ database/migrations/2024_02_29_092643_create_social_networks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('social_networks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id');
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
             $table->string('link')->nullable();
             $table->timestamps();
         });
