64f84c7f1361732de216b176776586b47cb4971c
George Kalandadze <kalandadzeg777@gmail.com>
2024-03-02 02:11:44 +0400

    feat/make model and migration for resume

---
+++ app/Models/Resume.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Resume extends Model
+{
+    use HasFactory;
+
+    public function vacancy(): BelongsTo
+    {
+        return $this->belongsTo(Vacancy::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
